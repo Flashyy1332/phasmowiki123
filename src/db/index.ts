380d429334c84c1115b3250aeae52d16cefef157
Flashyy1332 <discard191@gmail.com>
2026-06-22 18:30:04 +0300

    змінений порт на 6543

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import { Pool } from 'pg';
 // Жодних конфігураційних файлів чи environment variables.
 export const pool = new Pool({
   host: 'db.mnsqbsxazooykgzmjzug.supabase.co',
-  port: 5432,
+  port: 6543, // Змінено на 6543 для підтримки IPv4 на Vercel
   database: 'postgres',
   user: 'postgres',
   password: 'Database192837!(@*#&',
